eb73fe34b3f3f57be597660d383affd585b380cb
sketch25 <sket.ch.life725@gmail.com>
2020-08-22 18:35:00 +0900

    create_comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :likes
-  has_many :post_tags
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
   has_many :tags, through: :posts_tags
 
   validates :title, :content, presence: true
